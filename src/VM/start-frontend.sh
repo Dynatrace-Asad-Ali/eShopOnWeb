22832dbdaa67b2c11a4a8592b4c5389df4e466e8
peter.hack <peter.hack@dynatrace.com>
2020-01-08 16:05:47 -0500

    new frontend script

---
+++ src/VM/start-frontend.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#############################################################
+# This script will build and start the frontend Nginx Proxy #
+# for eShopOnWeb                                            #
+#############################################################
+cd /home/eshopadmin/eShopOnWeb/src/Proxy/
+docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.dt.yml up -d
