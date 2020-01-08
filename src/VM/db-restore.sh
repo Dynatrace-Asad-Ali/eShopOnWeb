b4ad262fef4d2c34158c6bbaa42ac6f0a2e052d2
peter.hack <peter.hack@dynatrace.com>
2020-01-08 14:28:34 -0500

    updates for runuser

--- src/VM/db-restore.sh
+++ src/VM/db-restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-DOTNET_CLI_HOME=/tmp
+echo "DB Restore Context"
 cd /home/eshopadmin/eShopOnWeb/src/Web
+echo "changed directory to: $(pwd)"
 dotnet restore
 dotnet tool restore
 dotnet ef database update -c catalogcontext -p ../Infrastructure/Infrastructure.csproj -s Web.csproj
 dotnet ef database update -c appidentitydbcontext -p ../Infrastructure/Infrastructure.csproj -s Web.csproj
-echo "DB context configured"
