0e5c956a11c82f0b0586de98b350a013c316e2b4
peter.hack <peter.hack@dynatrace.com>
2020-01-08 08:03:26 -0500

    dotnet cli var for db

--- src/VM/db-restore.sh
+++ src/VM/db-restore.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/eshopadmin/eShopOnWeb/src/Web
+DOTNET_CLI_HOME=~
 dotnet restore
 dotnet tool restore
 dotnet ef database update -c catalogcontext -p ../Infrastructure/Infrastructure.csproj -s Web.csproj
