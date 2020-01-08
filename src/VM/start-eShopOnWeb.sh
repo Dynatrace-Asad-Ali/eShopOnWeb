adfddb1094167447002633d516f53d6bee5e8663
peter.hack <peter.hack@dynatrace.com>
2020-01-08 15:58:40 -0500

    bashified

--- src/VM/start-eShopOnWeb.sh
+++ src/VM/start-eShopOnWeb.sh
@@ -3,4 +3,3 @@ cd /home/eshopadmin/eShopOnWeb
 sed -i 's/localhost/localdb/g' /home/eshopadmin/eShopOnWeb/src/Web/appsettings.json
 docker-compose build
 docker-compose up -d
-echo "eShopOnWeb Started"
